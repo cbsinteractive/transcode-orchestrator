190a5504eb205e61ec983cb4a1144fe45960b520
Krystal Mejia <krystalmejia24@users.noreply.github.com>
2019-11-14 18:20:28 -0500

    remove video track dependency from get requests in bitmovin (#102)

--- provider/bitmovin/internal/container/types.go
+++ provider/bitmovin/internal/container/types.go
@@ -40,12 +40,14 @@ func int64Value(i *int64) int64 {
 	if i == nil {
 		return 0
 	}
+
 	return *i
 }
 
-func int32Value(i *int32) int32 {
+func dimensionToInt64(i *int32) int64 {
 	if i == nil {
 		return 0
 	}
-	return *i
+
+	return int64(*i)
 }
