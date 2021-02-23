92850e401d746e0d534f0de44ca317183ee045ab
as <as.utf8@gmail.com>
2021-02-23 15:02:59 -0800

    fix provider package

--- provider/fake_provider_test.go
+++ provider/fake_provider_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/cbsinteractive/transcode-orchestrator/config"
-	"github.com/cbsinteractive/transcode-orchestrator/db"
+	"github.com/cbsinteractive/transcode-orchestrator/job"
 )
 
 type fake struct {
@@ -12,11 +12,11 @@ type fake struct {
 	health error
 }
 
-func (fake) Create(context.Context, *job.Job) (*Status, error) { return nil, nil }
-func (fake) Status(context.Context, *job.Job) (*Status, error) { return nil, nil }
-func (fake) Cancel(context.Context, string) error              { return nil }
-func (f fake) Healthcheck() error                              { return f.health }
-func (f fake) Capabilities() Capabilities                      { return f.cap }
+func (fake) Create(context.Context, *job.Job) (*job.Status, error) { return nil, nil }
+func (fake) Status(context.Context, *job.Job) (*job.Status, error) { return nil, nil }
+func (fake) Cancel(context.Context, string) error                  { return nil }
+func (f fake) Healthcheck() error                                  { return f.health }
+func (f fake) Capabilities() Capabilities                          { return f.cap }
 
 func getFactory(err error, health error, cap Capabilities) Factory {
 	return func(*config.Config) (Provider, error) {
