45915f4f9d0858b991389eee362d797efcceb1d3
as <as.utf8@gmail.com>
2021-03-24 13:11:18 -0700

    add tags

--- av/job.go
+++ av/job.go
@@ -21,31 +21,30 @@ const (
 
 // Env contains configurations for the environment used while transcoding
 type Env struct {
-	Cloud       string
-	Region      string
-	InputAlias  string
-	OutputAlias string
-
-	Tags map[string]string
+	Cloud       string            `json:"cloud"`
+	Region      string            `json:"region"`
+	InputAlias  string            `json:"inputAlias"`
+	OutputAlias string            `json:"outputAlias"`
+	Tags        map[string]string `json:"tags,omitempty"`
 }
 
 // Job is a transcoding job
 type Job struct {
-	ID        string `json:"id"`
-	Name      string `json:"name"`
-	CreatedAt time.Time
-	Labels    []string
+	ID     string   `json:"id,omitempty"`
+	Name   string   `json:"name,omitempty"`
+	Labels []string `json:"labels,omitempty"`
 
-	Provider      string `json:"provider"`
-	ProviderJobID string
+	CreatedAt     time.Time `json:"createdAt"`
+	Provider      string    `json:"provider"`
+	ProviderJobID string    `json:"providerJobID"`
 
-	Input  File
-	Output Dir
+	Input  File `json:"input"`
+	Output Dir  `json:"output"`
 
-	Features Features
-	Env      Env
+	Features Features `json:"features,omitempty"`
+	Env      Env      `json:"env"`
 
-	ExtraFiles map[string]string
+	ExtraFiles map[string]string `json:"extraFiles,omitempty"`
 }
 
 func (j *Job) Asset(sidecar string) *File {
